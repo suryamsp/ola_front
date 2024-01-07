d5bda494044636f2e4e1cbbbf340be9bedaf4669
suryamsp <murugesansuryamsp@gmail.com>
2024-01-06 18:40:12 -0800

    view

--- src/sub_folder/View.jsx
+++ src/sub_folder/View.jsx
@@ -4,17 +4,35 @@ import { useNavigate, useParams } from "react-router-dom";
 export function View() {
 const navigate  = useNavigate();
 const { url } = useParams();
-const [moviepage, setmoviepage] = useState({});
+const [Update, setUpdate] = useState({});
+const [tripList, setTripList] = useState([]);
 useEffect(()=>{ 
-  fetch(`https://trip-backend-eight.vercel.app/Triplist/${url}`)
-   .then((data) => data.json())
-   .then((mvs) => console.log(mvs));},[url]);
+  fetch(`https://trip-backend-eight.vercel.app/Updatelist/${url}`)
+   .then((data) =>setUpdate(data));},[url]);
+   
+   const fetchData = async (url, setList, errorMessage) => {
+    try {
+      const response = await fetch(url);
+      const list = await response.json();
+      setList(list);
+    } catch (error) {
+      console.error(`Error fetching ${errorMessage}:`, error);
+    }
+  };
+  
+  const getlist = async () => {
+    await fetchData(`https://trip-backend-eight.vercel.app/Triplist/${url}`, setTripList, "Triplist");
+  };
 
+  useEffect(() => {
+    getlist();
+   
+  }, []);
   return (
     <div className="view_dev">
 
       <div>
-        <img src="./img/tirupathi.jpg" className="view_img" alt="Thirupathi" />
+        <img src={tripList.image} className="view_img" alt={Update.trip_name} />
       </div>
       <div className="view-body">
 
@@ -30,17 +48,17 @@ useEffect(()=>{
           <p className="left_side">Command</p>
         </div>
         <div className="view-con2">
-          <p>: Thirupathi </p>
-          <p>: Thirupathi / Andhra pradesh </p>
-          <p>: 12-10-2023 </p>
-          <p>: 12-10-2023 </p>
-          <p >: Chennai- Renigunta - kalahasti - Thirupathui </p>
-          <p>: 500 /per head </p>
+          <p>: {Update.trip_name} </p>
+          <p>: {Update.city} </p>
+          <p>: {Update.str_date} </p>
+          <p>: {Update.end_date} </p>
+          <p >: {Update.route}</p>
+          <p>: {Update.budjet}/per head </p>
           <p>: Trip Completed <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" color="green" fill="currentColor" className="bi bi-patch-check-fill" viewBox="0 0 16 16">
             <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
           </svg> </p>
-          <p>: Surya - venkatesan - Vijayakumar -Gnanavel </p>
-          <p>: Gnanavel is return to chennai not completed trip. because over crowd to  thirupathi </p>
+          <p>: {Update.member} </p>
+          <p>: {Update.command} </p>
         </div>
 
       </div>
