08edf81b61955147742e852c4cb513d5a702f5bd
suryamsp <murugesansuryamsp@gmail.com>
2024-01-06 19:17:05 -0800

    #

--- src/sub_folder/View.jsx
+++ src/sub_folder/View.jsx
@@ -4,40 +4,40 @@ import { useNavigate, useParams } from "react-router-dom";
 export function View() {
 const navigate  = useNavigate();
 const { url } = useParams();
-const [updateList, setUpdateList] = useState();
-const [tripList, setTripList] = useState([]);
+const [updateView, setUpdateView] = useState();
+const [tripView, setTripView] = useState([]);
 
 
-   const fetchData = async (url, setList, errorMessage) => {
+   const fetchData = async (url, setView, errorMessage) => {
     try {
       const response = await fetch(url);
       const list = await response.json();
-      setList(list);
+      setView(list);
     } catch (error) {
       console.error(`Error fetching ${errorMessage}:`, error);
     }
   };
   
-  const getlist = async () => {
-    await fetchData(`https://trip-backend-eight.vercel.app/Triplist/${url}`, setTripList, "Triplist");
+  const getview = async () => {
+    await fetchData(`https://trip-backend-eight.vercel.app/Triplist/${url}`, setTripView, "Triplist");
   };
-  const getupdatelist = async () => {
-    await fetchData(`https://trip-backend-eight.vercel.app/Updatelist/${url}`, setUpdateList, "Updatelist");
+  const getupdateview = async () => {
+    await fetchData(`https://trip-backend-eight.vercel.app/Updatelist/${url}`, setUpdateView, "Updatelist");
   };
   
 
 
   useEffect(() => {
-    getlist();
-    getupdatelist();
+    getview();
+    getupdateview();
    
-  }, []);
+  }, [url]);
 
   return (
     <div className="view_dev">
 
       <div>
-        <img src={tripList.image} className="view_img" alt={updateList.trip_name} />
+        <img src={tripView.image} className="view_img" alt={tripView.trip_name} />
       </div>
       <div className="view-body">
 
@@ -53,17 +53,17 @@ const [tripList, setTripList] = useState([]);
           <p className="left_side">Command</p>
         </div>
         <div className="view-con2">
-          <p>: {updateList.trip_name} </p>
-          <p>: {updateList.city} </p>
-          <p>: {updateList.str_date} </p>
-          <p>: {updateList.end_date} </p>
-          <p >: {updateList.route}</p>
-          <p>: {updateList.budjet}/per head </p>
+          <p>: {tripView.trip_name} </p>
+          <p>: {updateView.city} </p>
+          <p>: {updateView.str_date} </p>
+          <p>: {updateView.end_date} </p>
+          <p >: {updateView.route}</p>
+          <p>: {updateView.budjet}/per head </p>
           <p>: Trip Completed <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" color="green" fill="currentColor" className="bi bi-patch-check-fill" viewBox="0 0 16 16">
             <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
           </svg> </p>
-          <p>: {updateList.member} </p>
-          <p>: {updateList.command} </p>
+          <p>: {updateView.member} </p>
+          <p>: {updateView.command} </p>
         </div>
 
       </div>
